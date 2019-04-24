4c3e8b88bf06e539eecfac2adad5279bba6e7a79
Andrei Egorov <andreiegorov@Andreis-MacBook-Pro-2.local>
2019-04-23 22:51:32 -0700

    modify layouts, clean boilerplate, add toolbar

---
+++ components/ToolBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-toolbar dark color="blue">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-tooblar-title class="white--text">
+      <nuxt-link class="title" to="/"> iTunes Search</nuxt-link>
+    </v-tooblar-title>
+    <v-spacer></v-spacer>
+    <v-btn to="/">
+      <v-icon> refresh</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    name: "ToolBar"
+  }
+</script>
+
+<style scoped>
+  .title{
+    text-decoration: none !important;
+  }
+
+  .title:visited{
+    color: white;
+  }
+
+</style>
